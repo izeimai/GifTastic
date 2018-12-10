75105f405e5b96197202357754c28afdc0b045b5
Ize Imai <izeimai@gmail.com>
2018-12-10 18:31:34 -0500

    Adding more attributes to the gif image tags

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,10 +13,28 @@ for (var i = 0; i < topics.length; i++) {
     $("#buttonDisplay").append(button); // appends button to the buttonDisplay div
 }
 
+// onclick event handler for buttons
 $("button").on("click", function () {
     searchTerm = $(this).val().trim();
     $("#gifDisplay").empty();
     displayGif(); // ajax call
+});
+
+// onclick event handler for the gifts (to start and stop)
+$("img").on("click", function () {
+    // store state and url to local variables
+    var state = $(this).attr("gifState");
+    var dataStill = $(this).attr("data-still");
+    var dataActive = $(this).attr("data-active");
+    console.log(state, dataStill, dataActive)
+    if (state == "still") { // from still to active
+        $(this).attr("src", dataActive);
+        $(this).attr("gifState", "active")
+    } else { // active to still
+        $(this).attr("src", dataStill);
+        $(this).attr("gifState", "still")
+    }
+
 });
 
 function displayGif() {
@@ -29,11 +47,14 @@ function displayGif() {
     }).then(function (response) {
         console.log(response);
         for (var j = 0; j < response.data.length; j++) {
-            var imageUrl = response.data[j].images.fixed_height_still.url;
-            console.log(imageUrl);
-            var gif = $("<img>");
-            gif.attr("src", imageUrl);
-            gif.attr("alt", "gif image");
+            var stillUrl = response.data[j].images.fixed_height_still.url;
+            var activeUrl = response.data[j].images.fixed_height.url;
+            var gif = $("<img>"); // create image tag
+            gif.attr("src", stillUrl); // start off with src being the still version
+            gif.attr("gifState", "still"); // and start off with gifState attribute being still
+            gif.attr("data-still", stillUrl); // store the still version gif into 'data-still' attribute
+            gif.attr("data-active", activeUrl); // store the active version gif into 'data-active' attribute
+            gif.attr("alt", "gifimage"); // if gif link breaks, at least I'll see the alternative name text
             $("#gifDisplay").prepend(gif);
         }
 
