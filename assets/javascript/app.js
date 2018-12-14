9cb072cd878db345272955fea8ed2c7105e23c7d
Ize Imai <izeimai@gmail.com>
2018-12-13 19:34:26 -0500

    Added functionality, next step is bonus goals

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,17 +4,35 @@
 var topics = ["apple", "banana", "carrot", "doughnut", "eggplant", "pizza", "sushi"]
 var searchTerm;
 
-// Generates buttons for each index in topics array
-for (var i = 0; i < topics.length; i++) {
-    var button = $("<button>");
-    button.addClass("btn btn-info mr-1");
-    button.text(topics[i]);
-    button.val(topics[i]);
-    $("#buttonDisplay").append(button); // appends button to the buttonDisplay div
+function makeButtons() {
+    // Generates buttons for each index in topics array
+    for (var i = 0; i < topics.length; i++) {
+        var button = $("<button>");
+        button.addClass("btn btn-info mr-1");
+        button.text(topics[i]);
+        button.val(topics[i]);
+        $("#buttonDisplay").append(button); // appends button to the buttonDisplay div
+    }
 }
 
+makeButtons(); // default
+
+// onclick event handler for submit button
+$("#submitButton").on("click", function () {
+    // event.preventDefault(); // prevents the submit button default behavior but didn't need it here
+    var newTopic = $("#form").val().trim();
+    if (!topics.includes(newTopic)) { // won't add a new button if the newTopic is already in the topics array
+        topics.push(newTopic); // adding the new topic from the form to the topics array
+        $("#buttonDisplay").empty(); // remove the previous buttons
+        makeButtons(); // display the new updated array for buttons
+    } else {
+        alert("You already have this topic displayed")
+    }
+});
+
+
 // onclick event handler for buttons
-$("button").on("click", function () {
+$(document).on("click", "button", function () { // needs to be document because my buttons change if form is entered
     searchTerm = $(this).val().trim();
     $("#gifDisplay").empty();
     displayGif(); // ajax call
@@ -26,8 +44,7 @@ $(document).on("click", "#gif", function () {
     var state = $(this).attr("gifState");
     var dataStill = $(this).attr("data-still");
     var dataActive = $(this).attr("data-active");
-    
-    console.log(state, dataStill, dataActive)
+
     if (state == "still") { // from still to active
         $(this).attr("src", dataActive);
         $(this).attr("gifState", "active")
@@ -35,7 +52,6 @@ $(document).on("click", "#gif", function () {
         $(this).attr("src", dataStill);
         $(this).attr("gifState", "still")
     }
-
 });
 
 
@@ -47,18 +63,26 @@ function displayGif() {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
-        for (var j = 0; j < response.data.length; j++) {
+
+        for (var j = 0; j < response.data.length; j++) { //each gif
             var stillUrl = response.data[j].images.fixed_height_still.url;
             var activeUrl = response.data[j].images.fixed_height.url;
+            var rating = response.data[j].rating;
             var gif = $("<img>"); // create image tag
             gif.attr("id", "gif"); // adding an id of gif
             gif.attr("src", stillUrl); // start off with src being the still version
             gif.attr("gifState", "still"); // and start off with gifState attribute being still
             gif.attr("data-still", stillUrl); // store the still version gif into 'data-still' attribute
             gif.attr("data-active", activeUrl); // store the active version gif into 'data-active' attribute
-            gif.attr("alt", "gifimage"); // if gif link breaks, at least I'll see the alternative name text
-            $("#gifDisplay").prepend(gif);
-        }
+            gif.attr("alt", "gifimage"); // if gif link breaks, at least it'll show the alternative name text
+
+            // Putting together the gif with the rating and prepending to gifDisplay
+            var card = $("<div>");
+            card.addClass("card img-fluid"); // adding class for card 
+            card.append(gif);
+            card.append("<p>Rating: " + rating + "</p>");
+            $("#gifDisplay").prepend(card);
+        } // close for loop for each gif
+
     }); // close AJAX call
 }
